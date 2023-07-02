7f60b1f1366a70446c6b056cc54060bf10ebbb6b
Dinh Long <dinhlong881@gmail.com>
2023-07-02 22:11:43 +0700

    fix cannot post

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// See https://github.com/typicode/json-server#module
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("database.json");
@@ -6,9 +7,9 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 // Add this before server.use(router)
 server.use(
-  // Add custom route here if needed
   jsonServer.rewriter({
     "/api/*": "/$1",
+    "/blog/:resource/:id/show": "/:resource/:id",
   })
 );
 server.use(router);
